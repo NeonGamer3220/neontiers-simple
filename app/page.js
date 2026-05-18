6b49bd7883eeb0a41410b8d43be8cd17e1ebcc9d
NeonGamer3220 <neon.gamer322@gmail.com>
2026-05-19 01:12:03 +0200

    Move gamemode bar to touch mainCard by removing margins

--- app/page.js
+++ app/page.js
@@ -1170,7 +1170,7 @@ onClick={() => {
            border: 1px solid var(--border);
            border-radius: 24px;
            padding: 22px 28px 28px;
-           margin-top: 20px;
+           margin-top: 0;
            position: relative;
            box-shadow: 0 24px 72px #00000061;
            overflow: visible;
@@ -1182,7 +1182,7 @@ onClick={() => {
             margin: 0 auto;
             padding-left: 20px;
             padding-right: 20px;
-            margin-top: 32px;
+            margin-top: 0;
             position: relative;
             z-index: 1;
           }
