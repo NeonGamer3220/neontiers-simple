ddb54831de1a46129218e9dfeb00f4f23f93aba8
NeonGamer3220 <neon.gamer322@gmail.com>
2026-05-18 17:08:40 +0200

    Fix nav icons: use currentColor instead of white for consistent styling

--- app/page.js
+++ app/page.js
@@ -356,8 +356,8 @@ export default function Page() {
              </a>
            </li>
            <li>
-             <a className="navLink" href={DISCORD_INVITE} target="_blank" rel="noreferrer">
-               <svg className="navLinkIcon" viewBox="0 0 16 16" aria-hidden="true" fill="white">
+<a className="navLink" href={DISCORD_INVITE} target="_blank" rel="noreferrer">
+                <svg className="navLinkIcon" viewBox="0 0 16 16" aria-hidden="true" fill="currentColor">
                  <path d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019q.463-.63.818-1.329a.05.05 0 0 0-.01-.059l-.018-.011a9 9 0 0 1-1.248-.595.05.05 0 0 1-.02-.066l.015-.019q.127-.095.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.05.05 0 0 1 .053.007q.121.1.248.195a.05.05 0 0 1-.004.085 8 8 0 0 1-1.249.594.05.05 0 0 0-.03.03.05.05 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.2 13.2 0 0 0 4.001-2.02.05.05 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03 0 0 0-.02-.019m-8.198 7.307c-.789 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612m5.316 0c-.788 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612"/>
                </svg>
                Discord
@@ -372,10 +372,10 @@ export default function Page() {
              </a>
            </li>
            <li>
-             <button className="navLink" onClick={showInfo ? closeInfo : openInfo} type="button" aria-expanded={showInfo ? "true" : "false"}>
-               <svg className="navLinkIcon" viewBox="0 0 16 16" aria-hidden="true" fill="white">
-                 <circle cx="8" cy="8" r="7"/>
-                 <path d="M7.75 6.5h.5v.5h-.5zm0 2h.5v3h-.5z" fill="#0b0e14"/>
+<button className="navLink" onClick={showInfo ? closeInfo : openInfo} type="button" aria-expanded={showInfo ? "true" : "false"}>
+                <svg className="navLinkIcon" viewBox="0 0 16 16" aria-hidden="true" fill="currentColor">
+                  <circle cx="8" cy="8" r="7"/>
+                  <path d="M7.75 6.5h.5v.5h-.5zm0 2h.5v3h-.5z" fill="#0b0e14"/>
                </svg>
                Információ
              </button>
